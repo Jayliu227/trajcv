c15f8de304c070fbcbbcc2669acf9eea95ad9920
Jay <jl7895@nyu.edu>
2020-03-06 17:33:03 -0500

    Finished gae.

--- algorithms/gae.py
+++ algorithms/gae.py
@@ -1,19 +1,8 @@
 import scipy.signal
 
 
-def calc_gae(rewards, vs, gamma, lam):
+def calc_gae(rewards, vs, gamma=0.99, lam=0.95):
     """ calculate the generalized advantage """
-    pass
-
-# gamma = .2349
-# x = [1., 21., 1., 13., 11.]
-#
-# ans = []
-# for i in range(len(x)):
-#     ans.append(x[i])
-#     for p, k in enumerate(x[i+1:]):
-#         ans[-1] += pow(gamma, p + 1) * k
-#
-# print(ans)
-#
-# print(scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1])
+    assert (len(rewards) == len(vs) - 1)  # rewards from t = 1 to T, vs from t = 0 to T
+    td_res = rewards + gamma * vs[1:] - vs[:-1]
+    return scipy.signal.lfilter([1], [1, -gamma * lam], td_res[::-1], axis=0)[::-1]
